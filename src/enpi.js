63a85882d66a3e4aab9a6b2ceb4e55cd1c8d6049
Lucas Berrigan <leberrigan@gmail.com>
2026-05-12 10:22:51 -0300

    add condition if enpi cfg not present. Update build.yml repo

--- src/enpi.js
+++ src/enpi.js
@@ -87,10 +87,11 @@ class Enpi {
     }
     
     configure(cfg) {
+        if (typeof cfg !== "object" || Object.keys(cfg).length == 0) return console.log("enpi: missing config")
 
         console.log(`enpi: config: ${JSON.stringify(cfg)}`)
-        const sensorName = cfg.key.devType.split('-')[1] // e.g. "enpi-light" -> "light"
-        const sensorSched = cfg.schedule
+        const sensorName = cfg?.key.devType.split('-')[1] // e.g. "enpi-light" -> "light"
+        const sensorSched = cfg?.schedule
         const s = this.sensors[sensorName]
         if (!s.active) return
         if (!s) return console.log(`enpi: unknown sensor ${sensorName} in config`)
