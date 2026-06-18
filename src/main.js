a6b75e6633ef3d631253615f02ad71c8ecd18067
Lucas Berrigan <leberrigan@gmail.com>
2026-06-18 08:56:23 -0300

    Record pulse decoder in data files

--- src/main.js
+++ src/main.js
@@ -142,9 +142,11 @@ TheMatron.on("bfOut", (d) => {
 TheMatron.on("setParam", (s) => {
     AllOut.write(["S", s.time, s.port, s.par, s.val, s.errCode, s.err].join(',') + "\n")
 })
-// Record dongle type as an S line when a device is added
+// Record dongle type and radio path as S lines when a device is added
 TheMatron.on("devAdded", (dev) => {
-    AllOut.write(["S", Date.now()/1000, dev.attr.port, "device_type", dev.attr.type, 0, ""].join(',') + "\n")
+    const t = Date.now()/1000
+    AllOut.write(["S", t, dev.attr.port, "device_type", dev.attr.type, 0, ""].join(',') + "\n")
+    AllOut.write(["S", t, dev.attr.port, "radio", dev.attr.radio, 0, ""].join(',') + "\n")
 })
 // Propagate time to all data files
 TheMatron.on("gpsSetClock", (prec, elapsed) => {
